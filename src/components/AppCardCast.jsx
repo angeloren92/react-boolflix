4768fa46a550ca81e887c0ccb00ce3f33b134cf2
angeloren92 <angeloren92@gmail.com>
2026-03-27 17:34:16 +0100

    add cast list

--- src/components/AppCardCast.jsx
+++ src/components/AppCardCast.jsx
@@ -14,15 +14,21 @@ export default function AppCardCast({ id, media_type, genre_ids }) {
         fetch(`${apiUrl}3/${media_type}/${id}/credits?api_key=${envKey}`)
             .then(response => response.json())
             .then(data => {
-                const filteredCast = [data.cast.map(element => element.name)]
-                setCast(filteredCast)
+                const filteredCast = data.cast.map(element => element.name)
+                setCast(filteredCast.slice(0, 5))
             })
     }, [id])
 
 
     return (
         <>
-            {id}
+            <ul>
+                {
+                    cast.map(element => (
+                        <li>{element}</li>
+                    ))
+                }
+            </ul>
         </>
     )
 }
