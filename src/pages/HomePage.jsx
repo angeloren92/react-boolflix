6fe20b294a6899a9f5f79b79de6b5263f3801bcc
angeloren92 <angeloren92@gmail.com>
2026-03-27 10:26:41 +0100

    add img placeholder if poster_path = null

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -5,14 +5,15 @@ export default function HomePage() {
 
   const { envKey, apiUrl } = useContext(GlobalContext)
   const [tempSearchMovieTitle, setTempSearchMovieTitle] = useState('')
-  const [mediaType, setMediaType] = useState('multi')
+  const [searchMediaType, setSearchMediaType] = useState('multi')
   const [mediaSearchResults, setMediaSearchResults] = useState({ page: 1, results: [] })
+  const imgPlaceholder = 'https://placehold.co/500x700?text=IMG+NOT+FOUND'
 
   console.log(mediaSearchResults)
 
   function handleSearch(e) {
     e.preventDefault()
-    fetch(`${apiUrl}3/search/${mediaType}?api_key=${envKey}&query=${tempSearchMovieTitle}&page=${mediaSearchResults.page}`)
+    fetch(`${apiUrl}3/search/${searchMediaType}?api_key=${envKey}&query=${tempSearchMovieTitle}&page=${mediaSearchResults.page}`)
       .then(response => response.json())
       .then(data => {
         setMediaSearchResults({
@@ -40,7 +41,7 @@ export default function HomePage() {
       <div className="container">
         <form action="" onSubmit={handleSearch}>
           <input type="text" placeholder="Cerca Titolo Film" value={tempSearchMovieTitle} onChange={(e) => setTempSearchMovieTitle(e.target.value)} />
-          <select name="" id="" onChange={(e) => setMediaType(e.target.value)}>
+          <select name="" id="" onChange={(e) => setSearchMediaType(e.target.value)}>
             <option value="multi">Film e SerieTV</option>
             <option value="movie">Film</option>
             <option value="tv">SerieTV</option>
@@ -51,7 +52,7 @@ export default function HomePage() {
           {
             mediaSearchResults.results.map(({id, name, original_language, original_name, title, original_title, vote_average, poster_path}) => (
               <ul key={id} className="list-unstyled">
-                <li><img src={`https://image.tmdb.org/t/p/w500/${poster_path}`} alt="" /></li>
+                <li><img src={poster_path !== null ? `https://image.tmdb.org/t/p/w500/${poster_path}` : imgPlaceholder} alt={title} /></li>
                 <li>{title || name}</li>
                 <li>{original_title || original_name}</li>
                 <li><span className={`fi fi-${handleCoutryCode(original_language)}`}></span></li>
