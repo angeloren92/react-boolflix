92c76b5e23e92810c899de5d8f8e01abea79df2b
angeloren92 <angeloren92@gmail.com>
2026-03-27 12:22:51 +0100

    adj css navbar

--- src/components/AppHeader.jsx
+++ src/components/AppHeader.jsx
@@ -3,7 +3,7 @@ import { useContext } from "react"
 
 export default function AppHeader() {
 
-    const {
+  const {
     tempSearchMovieTitle,
     setTempSearchMovieTitle,
     setSearchMediaType,
@@ -14,20 +14,27 @@ export default function AppHeader() {
     <header>
       <nav className="navbar bg-dark navbar-expand" data-bs-theme="dark">
         <div className="container-fluid">
-          <a className="navbar-brand" href="#">BOOLFLIX</a>
-          <form action="" onSubmit={handleSearch}>
-            <input type="text" placeholder="Cerca Titolo Film" value={tempSearchMovieTitle} onChange={(e) => setTempSearchMovieTitle(e.target.value)} />
-            <select name="" id="" onChange={(e) => setSearchMediaType(e.target.value)}>
-              <option value="multi">Film e SerieTV</option>
-              <option value="movie">Film</option>
-              <option value="tv">SerieTV</option>
-            </select>
-            <button type="submit">CERCA</button>
-          </form>
+          <section className="d-flex justify-content-between w-100 flex-wrap mx-5">
+            <a className="navbar-brand fw-bolder fs-2 text-danger" href="#">BOOLFLIX</a>
+            <form action="" className="d-flex flex-wrap gap-2"
+              onSubmit={handleSearch}>
+              <div className="d-flex flex-nowrap py-2">
+                <input type="text" id="searchTitleInput" placeholder="Cerca Titolo" className="i btn btn-outline-light"
+                  value={tempSearchMovieTitle}
+                  onChange={(e) => setTempSearchMovieTitle(e.target.value)} />
+                <select name="mediaType" id="mediaType" className="btn btn-outline-light text-start"
+                  onChange={(e) => setSearchMediaType(e.target.value)}>
+                  <option value="multi">Film e SerieTV</option>
+                  <option value="movie">Film</option>
+                  <option value="tv">SerieTV</option>
+                </select>
+              </div>
+              <button id="submit" className="btn btn-outline-light flex-grow-1" type="submit">CERCA</button>
+            </form>
+          </section>
         </div>
       </nav>
     </header >
   )
 }
-
 
