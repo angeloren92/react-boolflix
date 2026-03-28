c1bd4221f6cef3bbccf0e63cd5c89df3cb183556
angeloren92 <angeloren92@gmail.com>
2026-03-28 17:44:19 +0100

    add genre category list

--- src/components/AppCardGenre.jsx
+++ src/components/AppCardGenre.jsx
@@ -1,24 +1,31 @@
 import { useContext } from "react"
 import { GlobalContext } from "../context/GlobalContext"
 
-export default function AppCardGenre({ id, genre_ids, media_type }) {
+export default function AppCardGenre({ genre_ids, media_type }) {
 
     const { genre } = useContext(GlobalContext)
+    let findedGenre = []
 
-    if (media_type === 'tv') {
-        const prova = genre_ids.filter(element => )
+    if (media_type === 'movie') {
+        findedGenre = genre_ids.map(element => {
+            return genre.movie.find(item => item.id === element ? item.name : null)
         })
-            console.log(prova)
-        }
+    } else if (media_type === 'tv') {
+        findedGenre = genre_ids.map(element => {
+            return genre.tv.find(item => item.id === element ? item.name : null)
+        })
+    }
 
+    console.log(findedGenre)
 
     return(
         <figure>
             <span><strong>Generi Associati :</strong></span>
             <ul>
                 {
-                    
-                    <li></li>
+                    findedGenre.map(element => (
+                        <li key={element.id}>{element.name}</li>
+                    ))
                 }
             </ul>
         </figure>
